a3685d4f46f06758cdaa06d38337b5083dceb8de
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-04-04 19:34:05 +0530

    Add initial HTML templates and configure Flask routes

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("def.html")
+@app.route('/certificates')
+def certificates():  # put application's code here
+    return render_template("certificates.html")
 
+@app.route('/logos')
+def logos():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
